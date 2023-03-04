57d89ec5945a944a8f52d952e0257c0861243771
KGrzeg <kupczykgrzeg@gmail.com>
2023-03-04 21:26:39 +0100

    Added cool index view

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,17 @@
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 
 import { UnauthorizedLayout } from "../components/Layout";
-import { Button } from "@mui/material";
+import profilePic from "../public/hello.png";
 
-export default function Home({ children }) {
+export default function Home() {
   return (
     <>
       <Head>
@@ -11,10 +19,30 @@ export default function Home({ children }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <UnauthorizedLayout>
-        <h1> hej wypożyczarka książek</h1>
-        <Button>Zaloguj się z googlem</Button>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          sx={{ minHeight: "100vh" }}
+        >
+          <Card>
+            <CardContent sx={{ textAlign: "center" }}>
+              <Image src={profilePic} alt="Kitty" />
+              <h2 style={{ maxWidth: "70%", margin: "auto" }}>
+                Książki mamy, wypożyczenia mamy, inni też mają książki, które
+                możesz im zabrać lub dać swoje.
+              </h2>
 
-        {children}
+              <Box sx={{ m: 4 }}>
+                <Button variant="contained" size="large">
+                  <Link href="/">Zaloguj się z googlem</Link>
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
       </UnauthorizedLayout>
     </>
   );
