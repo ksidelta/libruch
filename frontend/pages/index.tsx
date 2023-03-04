203763ab6879b663b8349dd7da7627edfa2ba94b
Jozef Piecyk <jpiecyk@evojam.com>
2023-03-04 15:32:19 +0100

    feat: add typescript

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -90,7 +90,7 @@ export default function Home() {
         code {
           background: #fafafa;
           border-radius: 5px;
-          padding: 0.75rem;
+          padding: 0.75rem; 
           font-size: 1.1rem;
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
