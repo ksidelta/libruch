f48cc9b3c1474bd132f63167861539f3ec1af19c
JarekIT <JaroslawDanielewiczIT@gmail.com>
2023-03-04 19:49:54 +0100

    Create CopyQueryHandler.kt

---
+++ backend/src/main/kotlin/com/ksidelta/libruch/modules/copy/CopyQueryHandler.kt
@@ -0,0 +1,25 @@
+package com.ksidelta.libruch.modules.copy
+
+import com.ksidelta.libruch.modules.kernel.Party
+import org.axonframework.queryhandling.QueryHandler
+import org.springframework.stereotype.Service
+
+@Service
+class QueryHandler(val copyReadModelRepository: CopyReadModelRepository) {
+    @QueryHandler
+    fun query(queryAllCopies: QueryAllCopies) =
+        copyReadModelRepository.findAll().toList()
+
+    @QueryHandler
+    fun query(queryByOwner: QueryByOwner) =
+        copyReadModelRepository.findAllByOwner(owner = queryByOwner.owner).toList()
+
+    @QueryHandler
+    fun query(queryByOwners: QueryByOwners) =
+        copyReadModelRepository.findAllByOwnerIn(owner = queryByOwners.owners).toList()
+
+}
+
+class QueryAllCopies() {}
+class QueryByOwner(val owner: Party) {}
+class QueryByOwners(val owners: Collection<Party>) {}
