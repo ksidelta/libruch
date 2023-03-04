7489c54fc6528f01f317794639b1189ff11ffe9b
KGrzeg <kupczykgrzeg@gmail.com>
2023-03-04 22:46:04 +0100

    Read dark mode from system preferences

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,12 +5,18 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 import { createContext, useMemo, useState } from "react";
+
+import useMediaQuery from "@mui/material/useMediaQuery";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 export const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
 export default function LibruchApp({ Component, pageProps }) {
-  const [mode, setMode] = useState<"light" | "dark">("light");
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const [mode, setMode] = useState<"light" | "dark">(
+    prefersDarkMode ? "dark" : "light"
+  );
   const colorMode = useMemo(
     () => ({
       toggleColorMode: () => {
