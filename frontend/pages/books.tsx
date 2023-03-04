14cc6a75e48ae31ff3ea589705e28c4f3b905fc6
Jozef Piecyk <jpiecyk@evojam.com>
2023-03-04 17:38:28 +0100

    feat: add book list with mock data

--- frontend/pages/books.tsx
+++ frontend/pages/books.tsx
@@ -1,9 +1,72 @@
 import { AuthorizedLayout } from "../components/Layout";
+import {
+  Avatar,
+  Container,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
+
+import DeleteIcon from "@mui/icons-material/Delete";
+
+interface Book {
+  id: string;
+  title: string;
+  author: string;
+  thumbnailUrl: string;
+}
 
 const Books = () => {
+  const myBooks: Book[] = [
+    {
+      id: "id1",
+      title: "Przeminęło z wiatrem",
+      author: "Józef Piecyk",
+      thumbnailUrl:
+        "https://media.harrypotterfanzone.com/deathly-hallows-us-childrens-edition-1050x0-c-default.jpg",
+    },
+    {
+      id: "id2",
+      title: "Harry Potter i komnata tajemnic",
+      author: "Janina Rowling",
+      thumbnailUrl:
+        "https://media.harrypotterfanzone.com/deathly-hallows-us-childrens-edition-1050x0-c-default.jpg",
+    },
+  ];
+
+  const handleRemove = (id: string) => {
+    console.log("Książkę usunę", id);
+  };
+
   return (
     <AuthorizedLayout>
-      <div>lista moich książek, dodawanie nowej książki, usuwanie książki</div>
+      <Container>
+        <h1>Moje książki</h1>
+        <List>
+          {myBooks.map(({ title, id, author, thumbnailUrl }) => (
+            <ListItem
+              style={{
+                border: "1px solid",
+                borderRadius: 8,
+                marginBottom: 8,
+              }}
+              secondaryAction={
+                <IconButton edge="end" aria-label="delete">
+                  <DeleteIcon onClick={() => handleRemove(id)} />
+                </IconButton>
+              }
+              key={id}
+            >
+              <ListItemAvatar>
+                <Avatar src={thumbnailUrl} />
+              </ListItemAvatar>
+              <ListItemText primary={title} secondary={author} />
+            </ListItem>
+          ))}
+        </List>
+      </Container>
     </AuthorizedLayout>
   );
 };
