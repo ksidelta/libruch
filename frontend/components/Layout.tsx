e73d534e1eb1ab8b6ff1568a5e0ac4d6e2b365c4
KGrzeg <kupczykgrzeg@gmail.com>
2023-03-04 21:26:18 +0100

    Move Navbar outside Layout

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,92 +1,7 @@
-import { useState, ChangeEvent, MouseEvent } from "react";
-
-import Container from "@mui/material/Container";
-import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
 
-const Navbar = () => {
-  const [auth, setAuth] = useState(true);
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-
-  const handleMenu = (event: MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Libruch
-          </Typography>
-          <Button color="inherit">Przeglądaj książki</Button>
-          <Button color="inherit">Przeglądaj organizacje</Button>
-          {(auth && (
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem>Moje konto</MenuItem>
-                <MenuItem
-                  onClick={() => {
-                    setAuth(false);
-                    handleClose();
-                  }}
-                >
-                  Wyloguj
-                </MenuItem>
-              </Menu>
-            </div>
-          ))}
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-};
+import Navbar from "./Navbar"
 
 const Footer = () => {
   return (
@@ -105,7 +20,7 @@ export const UnauthorizedLayout = ({ children }) => {
         justifyContent="center"
         alignItems="center"
       >
-        <Container maxWidth="lg">{children}</Container>
+        {children}
         <Footer />
       </Grid>
     </>
@@ -122,7 +37,7 @@ export const AuthorizedLayout = ({ children }) => {
         justifyContent="center"
         alignItems="center"
       >
-        <Container maxWidth="lg">{children}</Container>
+        {children}
         <Footer />
       </Grid>
     </>
---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,84 @@
+import { useState, MouseEvent } from "react";
+
+import Link from "next/link";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+
+const Navbar = () => {
+  const [auth, setAuth] = useState(true);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link href="/">Libruch</Link>
+          </Typography>
+          <Button color="inherit">
+            <Link href="/books">Przeglądaj książki</Link>
+          </Button>
+          <Button color="inherit">
+            <Link href="/borrowings">Przeglądaj organizacje</Link>
+          </Button>
+          {auth && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem>Moje konto</MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    setAuth(false);
+                    handleClose();
+                  }}
+                >
+                  Wyloguj
+                </MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navbar;
