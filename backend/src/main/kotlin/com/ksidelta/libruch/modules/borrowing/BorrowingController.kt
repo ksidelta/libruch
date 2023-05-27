8fa5270fae04c6d1b239bfd0f9a3329f428ed35b
wiktorele <wiktor.ucinski@gmail.com>
2023-05-27 15:07:23 +0200

    Add /api request path

--- backend/src/main/kotlin/com/ksidelta/libruch/modules/borrowing/BorrowingController.kt
+++ backend/src/main/kotlin/com/ksidelta/libruch/modules/borrowing/BorrowingController.kt
@@ -12,6 +12,7 @@ import org.springframework.web.server.ResponseStatusException
 import java.util.*
 
 @RestController
+@RequestMapping(path = ["/api"])
 class BorrowingController(
     val commandGateway: CommandGateway
 ) {
