68c31d97d0de8eb59cb41246566595b802017920
JarekIT <jaroslawdanielewiczit@gmail.com>
2023-05-27 20:24:38 +0200

    Update CopyQueryHandler.kt

--- backend/src/main/kotlin/com/ksidelta/libruch/modules/copy/CopyQueryHandler.kt
+++ backend/src/main/kotlin/com/ksidelta/libruch/modules/copy/CopyQueryHandler.kt
@@ -3,6 +3,8 @@ package com.ksidelta.libruch.modules.copy
 import com.ksidelta.libruch.modules.kernel.Party
 import org.axonframework.queryhandling.QueryHandler
 import org.springframework.stereotype.Service
+import java.util.UUID
+import kotlin.jvm.optionals.getOrNull
 
 @Service
 class CopyQueryHandler(val copyReadModelRepository: CopyReadModelRepository) {
@@ -25,9 +27,14 @@ class CopyQueryHandler(val copyReadModelRepository: CopyReadModelRepository) {
             titleFragment = queryByOwners.titleFragment
         ).toList()
 
+    @OptIn(ExperimentalStdlibApi::class)
+    @QueryHandler
+    fun query(queryByIsbn: QueryByIsbn) =
+        copyReadModelRepository.findByIsbn(isbn = queryByIsbn.isbn).getOrNull()
 }
 
 class QueryAllCopies() {}
 class QueryByOwner(val owner: Party) {}
 class QueryByOwners(val owners: Collection<Party>) {}
 class QueryByIsbn(val isbn: String) {}
+class QueryByOrganisationAndTitleFragment(val organisationId: UUID, val titleFragment: String) {}
