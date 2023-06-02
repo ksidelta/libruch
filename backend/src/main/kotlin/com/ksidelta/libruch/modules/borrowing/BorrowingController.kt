9032ff7331d14de79a21493eb58aec4194dc2641
Dastin Sandura <d.kuwalek@gmail.com>
2023-06-02 09:15:17 +0200

    WIP

--- backend/src/main/kotlin/com/ksidelta/libruch/modules/borrowing/BorrowingController.kt
+++ backend/src/main/kotlin/com/ksidelta/libruch/modules/borrowing/BorrowingController.kt
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
 import java.util.*
--- backend/src/main/kotlin/com/ksidelta/libruch/modules/borrowing/BorrowingReadModel.kt
+++ backend/src/main/kotlin/com/ksidelta/libruch/modules/borrowing/BorrowingReadModel.kt
@@ -1,6 +1,8 @@
 package com.ksidelta.libruch.modules.borrowing;
 
+import com.ksidelta.libruch.modules.example.BookState
 import org.axonframework.eventhandling.EventHandler
+import org.axonframework.queryhandling.QueryHandler
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
@@ -9,20 +11,44 @@ import javax.persistence.Entity
 import javax.persistence.Id
 
 @Service
-class BorrowingEventProcessor(val borrowingReadModelRepository: BorrowingReadModelRepository) {
+class BorrowingEventProcessor(
+    val borrowingReadModelRepository: BorrowingReadModelRepository,
+    val borrowingCopyReadModelRepository: BorrowingCopyReadModelRepository
+) {
     @EventHandler
-    fun handle(event: RegisteredNewBorrowing) =
+    fun handle(event: RegisteredNewBorrowing) {
+
         borrowingReadModelRepository.save(
             BorrowingOfferModel(
-                id =  event.uuid,
+                id = event.uuid,
                 isbn = event.isbn
             )
         )
+
+        borrowingCopyReadModelRepository.save(BorrowingCopyModel(
+            id = event.uuid,
+            isbn = event.isbn,
+            status = BookState.BORROWED,
+            userId = event.userId
+
+        ))
+    }
+
+    @QueryHandler
+    fun queryBorrowedBooks(event: QueryBorrowedBooks): List<BorrowingCopyModel> =
+        borrowingCopyReadModelRepository.findByUserId(event.userUUID)
 }
+
+data class QueryBorrowedBooks(val userUUID: UUID)
 
 @Repository
 interface BorrowingReadModelRepository : CrudRepository<BorrowingOfferModel, UUID> {
     fun findByIsbn(isbn: String): BorrowingOfferModel?
+}
+
+@Repository
+interface BorrowingCopyReadModelRepository : CrudRepository<BorrowingCopyModel, UUID> {
+    fun findByUserId(userUUID: UUID): List<BorrowingCopyModel>
 }
 
 @Entity(name = "borrowing_offer_model")
@@ -30,4 +56,13 @@ data class BorrowingOfferModel(
     @Id
     val id: UUID,
     val isbn: String
+)
+
+@Entity(name = "borrowed_copy_model")
+data class BorrowingCopyModel(
+    @Id
+    val id: UUID,
+    val isbn: String,
+    val status: BookState,
+    val userId: UUID
 )
