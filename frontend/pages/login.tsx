571a16c469f81fb0ef7064310b9a41c36201dea6
Jozef Piecyk <jpiecyk@evojam.com>
2023-08-26 15:14:24 +0200

    feat: add authorization

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,11 +1,56 @@
+import Head from "next/head";
+import Image from "next/image";
+
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+
 import { UnauthorizedLayout } from "../components/Layout";
+import profilePic from "../public/hello.png";
+import ThemeToggler from "../components/ThemeToggler";
+import { useAuth } from "../features/auth/AuthProvider";
+import Link from "next/link";
 
-const Login = () => {
+export default function Home() {
+  const {} = useAuth();
+
   return (
-    <UnauthorizedLayout>
-      <div>przycisk googla, który przekierowuje na backend</div>
-    </UnauthorizedLayout>
-  );
-};
+    <>
+      <Head>
+        <title>Libruch</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <UnauthorizedLayout>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          sx={{ minHeight: "100vh" }}
+        >
+          <Card>
+            <CardContent sx={{ textAlign: "center" }}>
+              <Image src={profilePic} alt="Kitty" />
+              <h2 style={{ maxWidth: "70%", margin: "auto" }}>
+                Książki mamy, wypożyczenia mamy, inni też mają książki, które
+                możesz im zabrać lub dać swoje.
+              </h2>
 
-export default Login;
+              <Box sx={{ m: 4 }}>
+                <Button variant="contained" size="large">
+                  {/* Link is not used on purpose since /auth/login is not Next rendered  */}
+                  <a href="/auth/login/oauth2/google">Zaloguj się z googlem</a>
+                </Button>
+              </Box>
+
+              <ThemeToggler />
+            </CardContent>
+          </Card>
+        </Grid>
+      </UnauthorizedLayout>
+    </>
+  );
+}
